2de1f82505f40d5a6a6f0199cd3aac013bd1d4c9
Khalid Elassaad <khalidelassaad@gmail.com>
2023-03-17 15:42:43 -0700

    package: install springboot and want to die

---
+++ backend/src/test/java/gkke/sickchessappbro/SickchessappbroApplicationTests.java
@@ -0,0 +1,13 @@
+package gkke.sickchessappbro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SickchessappbroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
