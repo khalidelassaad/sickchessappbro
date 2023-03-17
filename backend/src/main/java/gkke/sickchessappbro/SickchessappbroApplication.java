683d3903358cdf7dbe05d2196ec59c9d6a9684c7
Khalid Elassaad <khalidelassaad@gmail.com>
2023-03-17 16:01:44 -0700

    code: springboot works omg it's sooooo good

--- backend/src/main/java/gkke/sickchessappbro/SickchessappbroApplication.java
+++ backend/src/main/java/gkke/sickchessappbro/SickchessappbroApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SickchessappbroApplication {
 
 	public static void main(String[] args) {
