89a32c4ce8020ba0c3bb3ba0f82e92bdb2e6e05b
Khalid Elassaad <khalidelassaad@gmail.com>
2023-03-17 15:57:52 -0700

    code: quickstarts hello world 2m25s still building

--- backend/src/main/java/gkke/sickchessappbro/SickchessappbroApplication.java
+++ backend/src/main/java/gkke/sickchessappbro/SickchessappbroApplication.java
@@ -2,6 +2,9 @@ package gkke.sickchessappbro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SickchessappbroApplication {
@@ -10,4 +13,8 @@ public class SickchessappbroApplication {
 		SpringApplication.run(SickchessappbroApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
